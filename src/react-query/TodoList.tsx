c77145c7662555c3b80edcd622db60165a35008e
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-05-31 16:51:11 +0900

    Initial commit

---
+++ src/react-query/TodoList.tsx
@@ -0,0 +1,40 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+interface Todo {
+  id: number;
+  title: string;
+  userId: number;
+  completed: boolean;
+}
+
+const TodoList = () => {
+  const fetchTodos = () =>
+    axios
+      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.data);
+
+  const { data } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchTodos,
+  });
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/todos");
+  }, []);
+
+  // if (error) return <p>{error}</p>;
+
+  return (
+    <ul className="list-group">
+      {data?.map((todo) => (
+        <li key={todo.id} className="list-group-item">
+          {todo.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
