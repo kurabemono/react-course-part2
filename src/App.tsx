671493f4c096f7af0fd68602df9681f04f1abe29
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-06-07 10:13:24 +0900

    Ch3-5 Share state using React Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import "./App.css";
 import TaskList from "./state-management/TaskList";
 import LoginStatus from "./state-management/LoginStatus";
+import { useReducer } from "react";
+import taskReducer from "./state-management/reducers/tasksReducer";
+import NavBar from "./state-management/NavBar";
+import HomePage from "./state-management/HomePage";
+import TasksContext from "./state-management/contexts/tasksContext";
 
 function App() {
-  return <LoginStatus />;
+  const [tasks, dispatch] = useReducer(taskReducer, []);
+  return (
+    <>
+      <TasksContext.Provider value={{ tasks, dispatch }}>
+        <NavBar />
+        <HomePage />
+      </TasksContext.Provider>
+    </>
+  );
 }
 
 export default App;
