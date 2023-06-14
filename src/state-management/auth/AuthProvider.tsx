b47e2623202ca3a6a8501fbc075866b67984c1e6
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-06-14 17:06:36 +0800

    Ch3-12 Exer. Organize code

---
+++ src/state-management/auth/AuthProvider.tsx
@@ -0,0 +1,38 @@
+import { ReactNode, useReducer } from "react";
+import AuthContext from "./authContext";
+
+interface LoginAction {
+  type: "LOGIN";
+  user: string;
+}
+
+interface LogoutAction {
+  type: "LOGOUT";
+}
+
+export type AuthAction = LoginAction | LogoutAction;
+
+const authReducer = (user: string, action: AuthAction): string => {
+  switch (action.type) {
+    case "LOGIN":
+      return action.user;
+    case "LOGOUT":
+      return "";
+  }
+};
+
+interface Props {
+  children: ReactNode;
+}
+
+const AuthProvider = ({ children }: Props) => {
+  const [user, dispatch] = useReducer(authReducer, "");
+
+  return (
+    <AuthContext.Provider value={{ user, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
--- src/state-management/auth/LoginStatus.tsx
+++ src/state-management/auth/LoginStatus.tsx
@@ -1,7 +1,8 @@
-import useAuth from "./hooks/useAuth";
+import useAuth from "./useAuth";
 
 const LoginStatus = () => {
   const { user, dispatch } = useAuth();
+
   if (user)
     return (
       <>
